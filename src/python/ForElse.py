31a6484092e512a30cbc5f93db0ae5f54d572c6f
Derick Beckwith <derick.beckwith@gmail.com>
2017-10-16 22:29:28 -0400

    Add more ForElse examples

--- src/python/ForElse.py
+++ src/python/ForElse.py
@@ -2,13 +2,15 @@
 
 # ForElse.py
 
-names = [] #'yoyo', 'yuki', 'zaphod', 'xeno', 'trillion', 'tinu']        #(A)  
+names = ['yoy', 'yuki', 'zaphod', 'xeno', 'trillion', 'tinu']        #(A)  
 
 for name in names[:]:                                                 #(B)
     if 1< len(name) <= 3:                                             #(C)
         continue                                                      #(D)
     if len(name) > 4:                                                 #(E)
-        names.remove( name )                                          #(F)
+        names.remove( name )  
+        print "> 4"
+        break                                        #(F)
     if len(name) == 1:                                                #(G)
         print "one letter names not allowed -- good bye"              #(H)
         break                                                         #(I)
@@ -16,3 +18,18 @@ for name in names[:]:                                                 #(B)
 else:                                                                 #(K)
     print "names is empty list"               # ['yoyo', 'yuki', 'xeno', 'tinu']      #(L)
 
+numbers = [1, 2, 3, 4, 5]
+
+for num in numbers:
+    if num == 7:
+        print "number found"
+        break
+else:
+    print "number not found"
+
+def findNumber(numbers, find):
+    for num in numbers:
+        if num == find:
+            return True
+
+print findNumber(numbers, 10)
