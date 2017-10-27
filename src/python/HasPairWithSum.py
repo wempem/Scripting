47805897fbfd3b668fbeb9e66cecda58cfe93713
Derick Beckwith <derick.beckwith@gmail.com>
2017-10-27 10:35:21 -0400

    Add more test cases to HasPairWithSum.py

--- src/python/HasPairWithSum.py
+++ src/python/HasPairWithSum.py
@@ -1,18 +1,14 @@
-
-list1 = [1, 2, 3, 9]
-
-list2 = [1, 2, 4, 4]
-
-num = 8
-
 def HasPairWithSum(numbers, summation):
     comps = set()
     for number in numbers:
         if number in comps:
-            return number, summation - number
+            return summation - number, number
         comps.add(summation - number)
 
-print HasPairWithSum(list1, num)
-print HasPairWithSum(list2, num)
+list1 = [1, 2, 3, 9]
+list2 = [1 ,2, 4, 4]
+list3 = [3, 1, 7, 19, 87, 22, 41, 33, 14, 11, 2]
 
-
+print list1.__str__() + ", find 2 that sum to 8 => " + HasPairWithSum(list1, 8).__str__()
+print list2.__str__() + ", find 2 that sum to 8 => " + HasPairWithSum(list2, 8).__str__()
+print list3.__str__() + ", find 2 that sum to 17 => " + HasPairWithSum(list3, 17).__str__()
